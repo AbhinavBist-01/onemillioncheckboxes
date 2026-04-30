c484e42767b59dc7bbd6363e61501f0e0b1dc0ab
Abhinav <abhinavbist05@gmail.com>
2026-04-30 09:40:03 +0530

    Socket io server initialised

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 import http from "node:http";
+import path from "node:path";
+
 import express from "express";
+import { Server } from "socket.io";
 
 async function main() {
+  const PORT = process.env.PORT ?? 8000;
+
   const app = express();
   const server = http.createServer(app);
 
-  const PORT = process.env.PORT ?? 8000;
+  const io = new Server();
+  io.attach(server);
 
+  //Socket Handler
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected: ", socket.id);
+
+    socket.on("client:checkbox:change", (data) => {
+      console.log(`Socket : [${socket.id}]:client:checkbox:change`, data);
+    });
+  });
+
+  // Express
+  app.use(express.static(path.resolve("./public")));
   app.get("/health", (req, res) => {
     res.json({ healthy: true });
   });
 
-  server.listen(PORT, (req, res) => {
+  server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 }
